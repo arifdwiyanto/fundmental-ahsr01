2ec8ea7bd0240dbe206abd0ddf7b15f30d26ddfe
arifdwiyanto <arif.dwiyanto94@gmail.com>
2025-09-03 21:06:20 +0700

    feat (exercise day03) : add task 2

--- Day03/exercise_3.ts
+++ Day03/exercise_3.ts
@@ -4,5 +4,19 @@ let mNumber: number = 9;
 
 // Add FOR loop statement to print multiplication table
 for (let i: number = 1; i <= 10; i++) {
-  console.log(`${mNumber} x ${i}`);
+    console.log(`${mNumber} x ${i}`);
+}
+
+// 2. Check whether a string is a palindrom or not
+// Define variable checkName as input
+let checkName: string = "madam";
+
+// Reverse name using split, reverse, and join function
+let reverseName: string = checkName.split("").reverse().join("");
+
+// Check whether chechName is same as reverseName or not
+if (checkName === reverseName) {
+    console.log(`${checkName} is palindrom name`);
+} else {
+    console.log(`${checkName} is not palindrom name`);
 }
