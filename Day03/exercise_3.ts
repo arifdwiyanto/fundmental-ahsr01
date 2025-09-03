5b9db97fd5c1afa8c9348d69ce005d9663215167
arifdwiyanto <arif.dwiyanto94@gmail.com>
2025-09-03 20:52:03 +0700

    feat (exercise day03) : task 1

---
+++ Day03/exercise_3.ts
@@ -0,0 +1,8 @@
+// 1. Display multiplication table of a given number
+// Define variable mNumber as input
+let mNumber: number = 9;
+
+// Add FOR loop statement to print multiplication table
+for (let i: number = 0; i < mNumber; i++) {
+  console.log(`${mNumber} x ${i}`);
+}
